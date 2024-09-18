2c640e47b9eb7979fb54bcb3a87b57ee7576608f
tocaku <bogiveler@gmail.com>
2024-09-18 22:54:43 +0300

    first commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "GamesVault",
+  url: "https://gamesvault.vercel.app",
+  description:
+    "Free and open source videogame all in one application. Created for players to bring the best experience for gamers.",
+  author: "Tocaku",
+  links: {
+    github: "https://github.com/tocaku",
+    personalSite: "https://github.com/tocaku",
+    mail: "mailto:bogiveler@gmail.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
